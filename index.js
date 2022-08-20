0707e98f3f07d94539765dda679d2e7be0feea61
sanchez-cristian <cpi.info.i@gmail.com>
2022-08-20 15:43:10 +0200

    Add comments to code

--- index.js
+++ index.js
@@ -1,35 +1,39 @@
 const express = require('express');
 var cors = require('cors')
-const path = require('path');
 
 const app = express();
 app.use(cors()); 
-const port = process.env.PORT || 5001;
 
-// sendFile will go here
+// use port 3000 unless there exists a preconfigured port on a server (e.g. Heroku)
+const port = process.env.PORT || 3017;
+ 
 app.get('/', function(req, res) {
     const getIP = (req) => {
         const conRemoteAddress = req.connection?.remoteAddress; // deprecated
         
-        const sockRemoteAddress = req.socket?.remoteAddress; // replacement for req.connection
+        const sockRemoteAddress = req.socket?.remoteAddress; // replacement for req.connection -> on localhost this is the correct one
 
         const xRealIP = req.headers['x-real-ip']; // some platforms use x-real-ip
 
-        const xForwardedForIP = (() => {
+        const xForwardedForIP = (() => { 
           const xForwardedFor = req.headers['x-forwarded-for']; // most proxies use x-forwarded-for
-          if (xForwardedFor) {
+          if (xForwardedFor) { 
             // The x-forwarded-for header can contain a comma-separated list of IP's
             // Further, some are comma separated with spaces, so whitespace is trimmed.
             const ips = xForwardedFor.split(',').map(ip => ip.trim()); 
-            console.log(ips); 
             return ips[0]; 
-          }
-        })()
+          } 
+        })
 
-        return xForwardedForIP || xRealIP || sockRemoteAddress || conRemoteAddress // prefer x-forwarded-for and fallback to the others
+        return xForwardedForIP() || xRealIP || sockRemoteAddress || conRemoteAddress // prefer x-forwarded-for and fallback to the others
     }
 
-    res.json({ ip:getIP(req)});
+    //returning and JSON object with the IP
+    res.json(
+      { 
+        ip: getIP(req) 
+      }  
+    );
 });
 
 app.listen(port);
